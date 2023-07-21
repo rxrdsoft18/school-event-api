c956b28f4183851ec199e0ead75d65206d9c0d0a
Richard J Inga Aliaga <rxrdsoft@gmail.com>
2023-07-20 19:44:51 -0500

    feat: add orm settings with environment varibles

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import ormConfig from './config/orm.config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [ormConfig],
+      expandVariables: true,
+      envFilePath: [`${process.env.NODE_ENV ?? ''}.env`],
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
